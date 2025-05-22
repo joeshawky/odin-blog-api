52cc63e742cda5adfcf8b5e634409dd809ca8e35
joeshawky <102739596+joeshawky@users.noreply.github.com>
2025-05-22 17:30:16 +0300

    converted inputs to objects

--- src/presentation/ApiServer.ts
+++ src/presentation/ApiServer.ts
@@ -1,25 +1,38 @@
-import express from "express";
+import express, { RequestHandler } from "express";
 import { CreateUserController } from "./CreateUserController";
 import { GetUsersController } from "./GetUsersController";
+import {} from "express"
+interface ApiServerRunProps {
+    port: number;
+    createUserController: CreateUserController;
+    getUsersController: GetUsersController;
+    loginUserController: any;
+    authMiddleware: RequestHandler
+}
 
 export class ApiServer {
-    public static async run(
-        port: number,
-        createUserController: CreateUserController,
-        getUsersController: GetUsersController,
-        LoginUserController: any
-    ): Promise<void> {
+    public static async run({
+        port,
+        authMiddleware,
+        createUserController,
+        getUsersController,
+        loginUserController,
+    }: ApiServerRunProps): Promise<void> {
         const app = express();
         app.use(express.json());
 
-        app.get("/", (req, res) => {
-            res.send("Hello World!");
+        app.get("/", authMiddleware, (req, res) => {
+            if ((req as any).user) {
+                res.send({message: "authenticated", user: (req as any).user})
+                return;
+            }
+            res.send("Not Authenticated");
         });
 
         app.post("/users", (req, res) => createUserController.handle(req, res));
         app.get("/users", (req, res) => getUsersController.handle(req, res));
-
-        app.post("/login", (req, res) => LoginUserController.handle(req, res));
+        
+        app.post("/login", (req, res) => loginUserController.handle(req, res));
 
         app.listen(port, () => {
             console.log(`Server is running on port ${port}`);
