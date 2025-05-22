6234ecba51401c98fb1e3cb028f931f37b3fcba0
joeshawky <102739596+joeshawky@users.noreply.github.com>
2025-05-23 00:15:44 +0300

    refactored use case

--- src/application/CreateUserUseCase.ts
+++ src/application/CreateUserUseCase.ts
@@ -6,20 +6,29 @@ import { IPasswordHasher } from "../shared/IPasswordHasher";
 import { IUseCase } from "../shared/IUseCase";
 import { IUseCaseResult } from "../shared/IUseCaseResult";
 
-export interface ICreateUserUseCase extends IUseCase<CreateUserDto, IUseCaseResult<DisplayUserDto>> {}
+export interface ICreateUserUseCase
+    extends IUseCase<CreateUserDto, IUseCaseResult<DisplayUserDto>> {}
+
+interface CreateUserUseCaseProps {
+    userRepo: IUserRepo;
+    passwordHasher: IPasswordHasher;
+}
 
 export class CreateUserUseCase implements ICreateUserUseCase {
-    constructor(
-        private userRepository: IUserRepo,
-        private _passwordHasher: IPasswordHasher
-    ) {}
+    private _userRepo: IUserRepo;
+    private _passwordHasher: IPasswordHasher;
+
+    constructor({ userRepo, passwordHasher }: CreateUserUseCaseProps) {
+        this._userRepo = userRepo;
+        this._passwordHasher = passwordHasher;
+    }
 
     async execute({
         name,
         email,
         password,
     }: CreateUserDto): Promise<IUseCaseResult<DisplayUserDto>> {
-        const existingUser = await this.userRepository.getByEmail(email);
+        const existingUser = await this._userRepo.getByEmail(email);
         if (existingUser) {
             return {
                 success: false,
@@ -40,13 +49,13 @@ export class CreateUserUseCase implements ICreateUserUseCase {
         );
         newUserResult.data.password = hashedPassword;
 
-        await this.userRepository.create(newUserResult.data);
+        await this._userRepo.create(newUserResult.data);
         // TODO: creating inside the user repo may return false, handle later.
 
         const userDto = DisplayUserDto.from(newUserResult.data);
         return {
             success: true,
-            data: userDto
-        } 
+            data: userDto,
+        };
     }
 }
