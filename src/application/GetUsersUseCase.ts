d12332a1fad2ee0a52dfe1f2455a6e843426cfa5
joeshawky <102739596+joeshawky@users.noreply.github.com>
2025-05-22 18:26:33 +0300

    renamed user display dto

--- src/application/GetUsersUseCase.ts
+++ src/application/GetUsersUseCase.ts
@@ -1,18 +1,17 @@
-import { User, UserDisplayDto } from "../domain/User";
 import { IUserRepo } from "../infrastructure/IUserRepo";
-import { IUserDisplayDto } from "../shared/Dtos/IUserDIsplay";
+import { DisplayUserDto } from "../shared/Dtos/DisplayUserDto";
 import { IUseCase } from "../shared/IUseCase";
 import { IUseCaseResult } from "../shared/IUseCaseResult";
 
 export interface IGetUsersUseCase
-    extends IUseCase<null, IUseCaseResult<IUserDisplayDto[]>> {}
+    extends IUseCase<null, IUseCaseResult<DisplayUserDto[]>> {}
 
 export class GetUsersUseCase implements IGetUsersUseCase {
     constructor(private _userRepository: IUserRepo) {}
 
-    async execute(): Promise<IUseCaseResult<IUserDisplayDto[]>> {
+    async execute(): Promise<IUseCaseResult<DisplayUserDto[]>> {
         const users = await this._userRepository.getAll();
-        const usersDtos = users.map((user) => UserDisplayDto.from(user));
+        const usersDtos = users.map((user) => DisplayUserDto.from(user));
         return {
             success: true,
             data: usersDtos,
