eaa8ff2adc3ad0f46f9bfb60ded12d0e5ba26d5f
joeshawky <102739596+joeshawky@users.noreply.github.com>
2025-05-22 17:31:21 +0300

    removed unnecessary object attributes

---
+++ src/application/VerifyUserUseCase.ts
@@ -0,0 +1,65 @@
+import { IUserRepo } from "../infrastructure/IUserRepo";
+import { IUseCaseResult } from "../shared/IUseCaseResult";
+import { IJwtService } from "../shared/IJwtService";
+import { IPasswordHasher } from "../shared/IPasswordHasher";
+import { IUseCase } from "../shared/IUseCase";
+
+interface ILoginUserDto {
+    email: string;
+    password: string;
+}
+
+type token = string;
+
+interface ILoginUserResult extends IUseCaseResult<token> {}
+
+export interface ILoginUserUseCase
+    extends IUseCase<ILoginUserDto, ILoginUserResult> {}
+
+export class LoginUserUseCase implements ILoginUserUseCase {
+    constructor(
+        private _userRepo: IUserRepo,
+        private _jwtService: IJwtService,
+        private _passwordHasher: IPasswordHasher
+    ) {}
+
+    async execute({
+        email,
+        password,
+    }: ILoginUserDto): Promise<ILoginUserResult> {
+        const user = await this._userRepo.getByEmail(email);
+        if (!user) {
+            return {
+                success: false,
+                errors: ["User not found"],
+            };
+        }
+
+        const isPasswordValid = await this._passwordHasher.verifyPassword(
+            password,
+            user.password
+        );
+        if (!isPasswordValid) {
+            return {
+                success: false,
+                errors: ["Invalid password"],
+            };
+        }
+
+        const tokenResult = this._jwtService.generateToken({
+            username: user.name,
+            email: user.email,
+        });
+        if (!tokenResult.isValid) {
+            return {
+                success: false,
+                errors: tokenResult.errors,
+            };
+        }
+
+        return {
+            success: true,
+            data: tokenResult.data
+        };
+    }
+}
