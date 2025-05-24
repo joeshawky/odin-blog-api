5ce058da266fe6abb29c0501443b67dcbc770253
joeshawky <102739596+joeshawky@users.noreply.github.com>
2025-05-24 23:01:24 +0300

    added auth middleware to comments post endpoint

--- src/presentation/ApiServer.ts
+++ src/presentation/ApiServer.ts
@@ -1,5 +1,5 @@
 import express, { RequestHandler } from "express";
-import {} from "express"
+import {} from "express";
 import { UserControllerHandlers } from "./controllers/UserController";
 import { PostControllerHandlers } from "./controllers/PostController";
 import { CommentControllerHandlers } from "./controllers/CommentController";
@@ -17,30 +17,38 @@ export class ApiServer {
         authMiddleware,
         userController,
         commentController,
-        postController
+        postController,
     }: ApiServerRunProps): Promise<void> {
         const app = express();
         app.use(express.json());
 
-        app.get("/", authMiddleware, (req, res) => {
-            if ((req as any).user) {
-                res.send({message: "authenticated", user: (req as any).user})
-                return;
-            }
-            res.send("Not Authenticated");
-        });
+        app.post("/users", (req, res) =>
+            userController.createUserHandler(req, res)
+        );
+        app.get("/users", (req, res) =>
+            userController.getUsersHandler(req, res)
+        );
+        app.get("/usersWithPosts", (req, res) =>
+            userController.getUsersWithPostsHandler(req, res)
+        );
 
-        app.post("/users", (req, res) => userController.createUserHandler(req, res));
-        app.get("/users", (req, res) => userController.getUsersHandler(req, res));
-        app.get("/usersWithPosts", (req, res) => userController.getUsersWithPostsHandler(req, res));
+        app.get("/posts", (req, res) =>
+            postController.getPostsHandler(req, res)
+        );
+        app.post("/posts", authMiddleware, (req, res) =>
+            postController.createPostHandler(req, res)
+        );
 
-        app.get("/posts", (req, res) => postController.getPostsHandler(req, res))
-        app.post("/posts", authMiddleware, (req, res) => postController.createPostHandler(req, res));
+        app.get("/comments", (req, res) =>
+            commentController.getCommentsHandler(req, res)
+        );
+        app.post("/comments", authMiddleware, (req, res) =>
+            commentController.createCommentHandler(req, res)
+        );
 
-        app.get("/comments", (req, res) => commentController.getCommentsHandler(req, res))
-        app.post("/comments", (req, res) => commentController.createCommentHandler(req, res))
-
-        app.post("/login", (req, res) => userController.loginUserHandler(req, res));
+        app.post("/login", (req, res) =>
+            userController.loginUserHandler(req, res)
+        );
 
         app.listen(port, () => {
             console.log(`Server is running on port ${port}`);
